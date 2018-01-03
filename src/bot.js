4828cff5d0c4ab1513e1ebdb792a5f19097a3404
Flo <x.client.ich@gmail.com>
2018-01-03 11:59:53 +0100

    Fixes
    - Ignored config.json
    - requiered react.js and config.json
    - removing msgs from buffer

--- src/bot.js
+++ src/bot.js
@@ -1,25 +1,28 @@
-const Discord = require("discord.js");
-const config = require("../config.example.json");
+const Discord = require('discord.js')
+const config = require('../config.json')
+const { react } = require('./react.js')
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
-const buffer = [];
+let buffer = []
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
-setInterval(() =>{
-    let msg = buffer[Math.floor(Math.random()*buffer.length)];
-    if(msg){
-        react(msg);
-    }
-}, config.interval/1000);
+setInterval(() => {
+  let random = Math.floor(Math.random() * buffer.length)
+  let msg = buffer[random]
+  if (msg) {
+    react(msg)
+    buffer.splice(random, 1)
+  }
+}, config.interval * 1000)
 
-client.on('message', (msg) => {
-    if(msg.channel.id === config.channelid){
-        buffer.push(msg);
-    }
-});
+client.on('message', msg => {
+  if (msg.channel.id === config.channelid) {
+    buffer.push(msg)
+  }
+})
 
-client.login(config.token);
+client.login(config.token)
--- src/react.js
+++ src/react.js
@@ -1,13 +1,11 @@
-const analysis = require('ml-sentiment')({lang: 'de'});
+const analysis = require('ml-sentiment')({ lang: 'de' })
 module.exports = {
-    react: (msg) =>{
-        let result = analysis.classify(msg.content);
-
-        if(result > 0){
-            msg.react("☺")
-        } else{
-            msg.react("☹")
-        }
-
-        }
-};
+  react: msg => {
+    let result = analysis.classify(msg.content)
+    if (result > 0) {
+      msg.react('☺')
+    } else {
+      msg.react('☹')
+    }
+  }
+}
